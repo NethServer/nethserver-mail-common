264f2643d13f12b3aa89a650ff5affe4a4dc4aea
Davide Principi <davide.principi@nethesis.it>
2012-07-31 15:51:16 +0200

    Added tranlsations (en)..

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Mail.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Mail.php
@@ -3,6 +3,7 @@
 $L['Update domain `${0}`'] = 'Update domain `${0}`';
 $L['Create a new domain'] = 'Create a new domain';
 $L['Messages to domain ${0}'] = 'Messages @${0}';
+$L['Messages to this domain'] = 'Messages to this domain';
 $L['TransportType_Reject_label'] = 'Reject';
 $L['TransportType_Relay_label'] = 'Relay to another server';
 
