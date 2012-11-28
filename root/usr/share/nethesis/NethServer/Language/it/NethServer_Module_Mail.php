056fa0b3db76fbd4ae8dd0684da83984f3459055
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-11-28 18:27:46 +0100

    Translation: refactor english and italian translation files #523

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Mail.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Mail.php
@@ -1,30 +1,26 @@
-<?php
+<?php 
 
-$L['Mail_Title'] = 'Email';
-$L['Mail_Description'] = 'Configura i servizi di posta elettronica';
-$L['Mail_Tags'] = 'mail email imap imap4 pop pop3 smtp ssl tls account posta elettronica alias pseudonimi dominio domini';
-
-$L['Domain_Title'] = 'Domini';
-$L['Update domain `${0}`'] = 'Modifica il dominio di posta `${0}`';
-$L['Create a new domain'] = 'Crea un nuovo dominio di posta';
-$L['Messages to domain ${0}'] = 'Messaggi @${0}';
-$L['Messages to this domain'] = 'Messaggi a questo dominio';
-$L['TransportType_Reject_label'] = 'Rifiuta';
-$L['TransportType_Relay_label'] = 'Passa ad un altro server';
-$L['domain_label'] = 'Dominio';
-
-$L['Message_Title'] = 'Messaggi';
-$L['${0} day'] = 'un giorno';
-$L['${0} days'] = '${0} giorni';
-$L['Queue message max size (${0})'] = 'Accetta messaggi fino a ${0}';
-$L['Queue message lifetime (${0})'] = 'Tenta l\'invio per ${0}';
-$L['SmartHostStatus_label'] = 'Invia tramite smarthost';
-$L['SmartHostName_label'] = 'Nome host';
-$L['SmartHostPort_label'] = 'Porta';
-$L['SmartHostUsername_label'] = 'Nome utente';
-$L['SmartHostPassword_label'] = 'Password';
-$L['SmartHostTlsStatus_label'] = 'Consenti connessione non cifrata';
-
-$L['Key_label'] = 'Dominio';
-
-$L['DisclaimerStatus_label'] = 'Aggiungi una nota legale in calce ai messaggi inviati';
+$L['${0} day'] = 'un giorno'; 
+$L['${0} days'] = '${0} giorni'; 
+$L['Create a new domain'] = 'Crea un nuovo dominio di posta'; 
+$L['DisclaimerStatus_label'] = 'Aggiungi una nota legale in calce ai messaggi inviati'; 
+$L['Domain_Title'] = 'Domini'; 
+$L['Key_label'] = 'Dominio'; 
+$L['Mail_Description'] = 'Configura i servizi di posta elettronica'; 
+$L['Mail_Tags'] = 'mail email imap imap4 pop pop3 smtp ssl tls account posta elettronica alias pseudonimi dominio domini'; 
+$L['Mail_Title'] = 'Email'; 
+$L['Message_Title'] = 'Messaggi'; 
+$L['Messages to domain ${0}'] = 'Messaggi @${0}'; 
+$L['Messages to this domain'] = 'Messaggi a questo dominio'; 
+$L['Queue message lifetime (${0})'] = 'Tenta l\'invio per ${0}'; 
+$L['Queue message max size (${0})'] = 'Accetta messaggi fino a ${0}'; 
+$L['SmartHostName_label'] = 'Nome host'; 
+$L['SmartHostPassword_label'] = 'Password'; 
+$L['SmartHostPort_label'] = 'Porta'; 
+$L['SmartHostStatus_label'] = 'Invia tramite smarthost'; 
+$L['SmartHostTlsStatus_label'] = 'Consenti connessione non cifrata'; 
+$L['SmartHostUsername_label'] = 'Nome utente'; 
+$L['TransportType_Reject_label'] = 'Rifiuta'; 
+$L['TransportType_Relay_label'] = 'Passa ad un altro server'; 
+$L['Update domain `${0}`'] = 'Modifica il dominio di posta `${0}`'; 
+$L['domain_label'] = 'Dominio'; 
