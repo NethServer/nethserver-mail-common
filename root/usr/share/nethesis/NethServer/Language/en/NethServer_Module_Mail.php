6a529584916d1cfeda6664a8d8c1226d25d8a50e
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-11-28 14:11:05 +0100

    Translation: update english translation #523

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Mail.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Mail.php
@@ -9,4 +9,33 @@ $L['TransportType_Relay_label'] = 'Relay to another server';
 
 $L['Key_label'] = 'Domain';
 
-$L['DisclaimerStatus_label'] = 'Append a legal note to sent messages';
+$L['DisclaimerStatus_label'] = 'Append a legal note to sent messages';$L['${0} day'] = '${0} day';
+$L['${0} days'] = '${0} days';
+$L['AddressAcl_label'] = 'Access rules by mail address';
+$L['allow From'] = 'allow From';
+$L['allow To'] = 'allow To';
+$L['BlockAttachmentCustomList_label'] = 'BlockAttachmentCustomList_label';
+$L['Confirm deletion of'] = 'Confirm deletion of';
+$L['deny From'] = 'deny From';
+$L['deny To'] = 'deny To';
+$L['DisclaimerText_label'] = 'DisclaimerText_label';
+$L['domain_label'] = 'Domains';
+$L['Domain_Title'] = 'Domains';
+$L['Mail_Description'] = 'Change mail configuration';
+$L['Mail_Title'] = 'Mail';
+$L['Message_Title'] = 'Messages';
+$L['New RW'] = 'New allow To';
+$L['New SB'] = 'New block From';
+$L['New SW'] = 'New allow From';
+$L['Queue message lifetime (${0})'] = 'Queue message lifetime (${0})';
+$L['Queue message max size (${0})'] = 'Queue message max size (${0})';
+$L['RelayHost_label'] = 'RelayHost_label';
+$L['SmartHostName_label'] = 'Hostname';
+$L['SmartHostPassword_label'] = 'Password';
+$L['SmartHostPort_label'] = 'Port';
+$L['SmartHostStatus_label'] = 'Send using a smarthost';
+$L['SmartHostTlsStatus_label'] = 'Allow uncrypted connections';
+$L['SmartHostUsername_label'] = 'Username';
+$L['SpamSubjectPrefixString_label'] = 'SpamSubjectPrefixString_label';
+
+$L['Mail_Tags'] = 'mail email imap imap4 pop pop3 smtp ssl tls account mail email e-mail alias pseudonymous domain domains';
